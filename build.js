929ce3c3e832a804cd67f09ffa62bfe287efe980
Titus Wormer <tituswormer@gmail.com>
2017-07-09 09:41:24 +0200

    Remove superfluous comments

--- build.js
+++ build.js
@@ -1,14 +1,10 @@
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
 var toJSON = require('plain-text-data-to-json');
 
-/* Read. */
 var doc = fs.readFileSync('data.txt', 'utf8');
 
-/* Transform. */
 var data = JSON.stringify(toJSON(doc), null, 2) + '\n';
 
-/* Write. */
 fs.writeFileSync('index.json', data);
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var fillers = require('./');
 
-/* Tests. */
 test('fillers', function (t) {
   t.equal(typeof fillers, 'object', 'should be an array #1');
   t.equal(Array.isArray(fillers), true, 'should be an array #2');
