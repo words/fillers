6dc2a31799bf650d8647c73c7763fe1905c57c28
Titus Wormer <tituswormer@gmail.com>
2021-04-05 16:00:55 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var test = require('tape')
 var fillers = require('.')
 
-test('fillers', function(t) {
+test('fillers', function (t) {
   t.equal(typeof fillers, 'object', 'should be an array #1')
   t.equal(Array.isArray(fillers), true, 'should be an array #2')
   t.notEqual(fillers.indexOf('basically'), -1, 'should contain words')
