04770e0c8ead040c474426e4d8f6e5ffe76bb285
Titus Wormer <tituswormer@gmail.com>
2022-11-06 12:40:09 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {fillers} from './index.js'
 
-test('fillers', function (t) {
-  t.equal(typeof fillers, 'object', 'should be an array #1')
-  t.equal(Array.isArray(fillers), true, 'should be an array #2')
-  t.notEqual(fillers.indexOf('basically'), -1, 'should contain words')
-
-  t.end()
+test('fillers', function () {
+  assert.equal(typeof fillers, 'object', 'should be an array #1')
+  assert.equal(Array.isArray(fillers), true, 'should be an array #2')
+  assert.notEqual(fillers.indexOf('basically'), -1, 'should contain words')
 })
